c3c147f5b7dd352f7987078574835eb809206616
jimenezrodriguezyurimar <yrodriguezj@iberoamericana.edu.co>
2020-10-17 19:48:43 -0500

    actividad 5 entregable

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('#parrafo').css("font-size", "30px");
+
+    $('.parrafo2').css("background","black").css("color","white");
+
+    $('li').css("background","red");
+
+});
+
